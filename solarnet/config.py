67c7bdc4efc4e1e7dcae34abd31836a66f9fb7c6
mohammadmozafari <mozafari.mmd@gmail.com>
2023-11-11 14:08:23 +0000

    Add paths to config file

--- solarnet/config.py
+++ solarnet/config.py
@@ -1,4 +1,7 @@
-
-
-UK_1M_DATASET_PATH = '/home/adelavar/uk-data-downloader/uk1M'
+UK_1M_FEATS_PATH = '/home/adelavar/Datasets/uk1M_features_dinov2_giant_3072.pickle'
+UK_1M_DATASET_PATH = '/home/adelavar/Datasets/uk1M'
+UK_20K_v2_DATASET_PATH = '/home/adelavar/Datasets/uk20K_v2'
 UK_20K_DATASET_PATH = '/home/adelavar/data/labeled_data_from_colab'
+
+LONDON_300_PATH = '/home/adelavar/Datasets/london300'
+LONDON_300_FEATS_PATH = '/home/adelavar/Datasets/london300_features_dinov2_giant_3072.pickle'
